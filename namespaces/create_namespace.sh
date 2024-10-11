433569683542fc6470625fc29e8cf3f402a40f39
joyson04 <joysonofficial2003@gmail.com>
2024-10-11 11:45:23 +0530

    add

---
+++ namespaces/create_namespace.sh
@@ -0,0 +1,13 @@
+kubectl create ns webapps
+
+kubectl apply -f namespaces.yml
+
+kubectl apply -f securt.yml -n webapps
+
+kubectl describe secret mysecretname -n webapps
+
+kubectl patch service post-blog-service -p '{"spec":{"selector":{"app":"post-blog", "version":"blue"}}}'
+
+kubectl scale deployment post-blog-blue --replicas=0
+
+kubectl set selector service post-blog-service version=blue
